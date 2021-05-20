ec9b83fce06830de8939c250f5de055a5a121b31
username <mantaoxu2@gmail.com>
2021-05-19 23:10:23 -0700

    done one more

--- 02-anagrams.js
+++ 02-anagrams.js
@@ -35,18 +35,60 @@ function firstAnagram(str1, str2) {
 function secondAnagram(str1, str2) {
   
   str1.split('').sort().join('')
+  str2.split('').sort().join('')
 
   return str1 !== str2;
 }
-console.log(secondAnagram("elvis", "lives")) // true
-console.log(secondAnagram("gizmo", "sally")) //false
+// console.log(secondAnagram("elvis", "lives")) // true
+// console.log(secondAnagram("gizmo", "sally")) //false
 
 
 function thirdAnagram(str1, str2) {
-  // Code goes here ....
+  let letterCountStr1 = {};
+  let letterCountStr2 = {};
+  let str1Arr = str1.split('');
+  let str2Arr = str2.split('');
+
+  str1Arr.forEach(el => {
+    letterCountStr1[el] = (letterCountStr1[el] || 0) + 1;
+  });
+  str2Arr.forEach(el => {
+    letterCountStr2[el] = (letterCountStr2[el] || 0) + 1;
+  });
+
+  const haveSameCount = function(obj1, obj2){
+    const obj1Length = Object.keys(obj1).length;
+    const obj2Length = Object.keys(obj2).length;
+
+    if(obj1Length === obj2Length) {
+      return Object.keys(obj1).every(
+        key => obj2.hasOwnProperty(key) && obj2[key] === obj1[key]
+      );
+    }
+    return false;
+  }
+  return haveSameCount(letterCounts1, letterCounts2);
+
+  
 }
+// console.log(secondAnagram("elvis", "lives")) // true
+// console.log(secondAnagram("gizmo", "sally")) //false
 
 
+
+// O(n) linear time
+// O(1) constant space (same logic as above)
 function fourthAnagram(str1, str2) {
   // Code goes here ....
+  let letterSums = {}
+
+  // If we do the exact same subractions for each letter in
+  // str2 as we do additions for str1, letter_sums will all be 0.
+  str1.split("").forEach(e => letterSums[e] = (letterSums[e] || 0) + 1);
+  str2.split("").forEach(e => letterSums[e] = (letterSums[e] || 0) - 1);
+
+  // It's a zero-sum game!
+  return Object.values(letterSums).every(sum => sum === 0);
 }
+console.log(secondAnagram("elvis", "lives")) // true
+// console.log(secondAnagram("gizmo", "sally")) //false
--- 03-two-sum.js
+++ 03-two-sum.js
@@ -1,23 +1,56 @@
+//O(n^2) quadratic time
+//O(1) constant space
 function badTwoSum(arr, targetSum) {
-  // Code goes here ...
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === targetSum) return true;
+    }
+  }
+  return false;
 }
+// console.log(badTwoSum([0, 1, 5, 7], 6)); //true;
+// console.log(badTwoSum([0, 1, 5, 7], 10)); //false;
+
 
 
 function okayTwoSum1(arr, targetSum) {
-  // Code goes here ...
+  arr.sort();   //sort the function first
+  let start = 0;
+  let end = arr.length -1;
+
+  while(start < end){
+    let currentSum = arr[start] + arr[end];
+    if(currentSum > targetSum){
+      end -= 1;
+    } else if(currentSum < targetSum){
+      start += 1;
+    } else{
+      return true;
+    }
+  }
+  return false;
+
 }
+console.log(okayTwoSum1([0, 1, 5, 7], 6)); //true;
+console.log(okayTwoSum1([0, 1, 5, 7], 10)); //false;
 
 
 function okayTwoSum2(arr, targetSum) {
   // Code goes here ...
 }
+// console.log(okayTwoSum2([0, 1, 5, 7], 6)); //true;
+// console.log(okayTwoSum2([0, 1, 5, 7], 10)); //false;
 
 
 function twoSum(arr, targetSum) {
   // Code goes here ...
 }
+// console.log(twoSum([0, 1, 5, 7], 6)); //true;
+// console.log(twoSum([0, 1, 5, 7], 10)); //false;
 
 
 function twoSumIndices(arr, targetSum) {
   // Code goes here ...
 }
+// console.log(twoSumIndices([0, 1, 5, 7], 6)); //true;
+// console.log(twoSumIndices([0, 1, 5, 7], 10)); //false;
