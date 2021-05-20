3c014f4e067f13981e6026b973fc3496fdda73fc
username <mantaoxu2@gmail.com>
2021-05-19 23:34:06 -0700

    finished

--- 03-two-sum.js
+++ 03-two-sum.js
@@ -31,26 +31,76 @@ function okayTwoSum1(arr, targetSum) {
   return false;
 
 }
-console.log(okayTwoSum1([0, 1, 5, 7], 6)); //true;
-console.log(okayTwoSum1([0, 1, 5, 7], 10)); //false;
+// console.log(okayTwoSum1([0, 1, 5, 7], 6)); //true;
+// console.log(okayTwoSum1([0, 1, 5, 7], 10)); //false;
 
-
+//O(nlogn) linearithmic time
+//O(n) linear space
+// another implementation using binary search
 function okayTwoSum2(arr, targetSum) {
-  // Code goes here ...
+  arr.sort();
+  for(let i = 0; i < arr.length; i++){
+    let num = arr[i];
+    let complement = targetSum - num;
+    let complementIndex = binarySearch(arr, complement);
+    if (complementIndex !== -1 && complementIndex !== i){
+      return true;
+    }
+  }
+  return false;
+}
+function binarySearch(array, target) {
+  let lowerIdx = 0;
+  let upperIdx = array.length - 1;
+  let midIdx;
+
+  while (lowerIdx <= upperIdx) {
+    midIdx = Math.floor((lowerIdx + upperIdx) / 2);
+    if (array[midIdx] < target) {
+      lowerIdx = midIdx + 1;
+    } else if (array[midIdx] > target) {
+      upperIdx = midIdx - 1;
+    } else {
+      return midIdx;
+    }
+  }
+
+  return -1;
 }
 // console.log(okayTwoSum2([0, 1, 5, 7], 6)); //true;
 // console.log(okayTwoSum2([0, 1, 5, 7], 10)); //false;
 
 
+//O(n) linear time
+//O(n) linear space
 function twoSum(arr, targetSum) {
-  // Code goes here ...
+  let complements = {}
+  for (let num of arr) {
+    let complement = targetSum - num
+    if (complements[complement]) return true
+    complements[num] = true
+  }
+
+  return false
 }
 // console.log(twoSum([0, 1, 5, 7], 6)); //true;
 // console.log(twoSum([0, 1, 5, 7], 10)); //false;
 
-
+//O(n) linear time
+//O(n) linear space
+// This can be easily adapted to also return the indices of the two numbers:
 function twoSumIndices(arr, targetSum) {
-  // Code goes here ...
+  let complements = {}
+  for (let i = 0; i < arr.length; i++) {
+    let num = arr[i]
+    let complement = targetSum - num
+    if (complements[complement]) {
+      let complementIndex = complements[complement]
+      return [i, complementIndex]
+    }
+    complements[num] = i
+  }
+  return [];
 }
-// console.log(twoSumIndices([0, 1, 5, 7], 6)); //true;
-// console.log(twoSumIndices([0, 1, 5, 7], 10)); //false;
+// console.log(twoSumIndices([0, 1, 5, 7], 6));  //[2, 1]
+// console.log(twoSumIndices([0, 1, 5, 7], 10));  // []
--- 04-execution-time-differences.js
+++ 04-execution-time-differences.js
@@ -1,23 +1,81 @@
+//O(n^2) quadratic time
+//O(n) linear space
 function myMin1a(list) {
-  // Code goes here ...
+  let minNum = null
+  for (let i = 0; i < list.length; i++){
+    let num = list[i];
+    let filterList = list.filter(filterNum => filterNum !== num);
+    if( filterList.every(item => item > num)){
+      minNum = num;
+    }
+  }
+  return minNum;
 }
+// console.log(myMin1a([ 0, 3, 5, 4, -5, 10, 1, 90 ])) // -5;
 
 
+//O(n^2) quadratic time
+//O(1) constant space
 function myMin1b(list) {
-  // Code goes here ...
+  for (let i = 0; i < list.length; i++) {
+    let num1 = list[i]
+    let min = true
+    for (let j = 0; j < list.length; j++) {
+      let num2 = list[j]
+      if (i === j) continue
+      if (num2 < num1) min = false
+    }
+    if (min) return num1
+  }
 }
+// console.log(myMin1b([ 0, 3, 5, 4, -5, 10, 1, 90 ])) // -5;
 
 
+//O(n) linear time
+//O(1) constant space
 function myMin2(list) {
-  // Code goes here ...
+  let minNum = list[0]
+
+  list.forEach(function (num) {
+    if (num < minNum) minNum = num
+  })
+
+  return minNum
 }
+// console.log(myMin2([ 0, 3, 5, 4, -5, 10, 1, 90 ])) // -5;
 
-
+//O(n^3) cubic time
+//O(n^3) cubic space
 function largestContiguousSubsum1(array) {
-  // Code goes here ...
+  let subArrays = []
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i; j < array.length; j++) {
+      subArrays.push(array.slice(i, j + 1))
+    }
+  }
+
+  let sumOfArrays = []
+  for (let elt of subArrays) {
+    sumOfArrays.push(elt.reduce((a, b) => a + b));
+  }
+  return Math.max(...sumOfArrays)
 }
+// console.log(largestContiguousSubsum1([ 0, 3, 5, 4, -5, 10, 1, 90 ])) // 108
 
 
+//O(n) linear time
+//O(1) constant space
 function largestContiguousSubsum2(array) {
-  // Code goes here ...
+  let largest = array[0]
+  let current = array[0]
+
+  for (let i = 1; i < array.length; i++) {
+    if (current < 0) current = 0
+    current += array[i]
+    if (current > largest) largest = current
+  }
+
+  return largest
 }
+console.log(largestContiguousSubsum2([ 0, 3, 5, 4, -5, 10, 1, 90 ])) // 108;
